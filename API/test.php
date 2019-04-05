baaaed3547503beef6d779f45a08015d0ad39289
Steven Shrewsbury <39103023+stshrewsburyDev@users.noreply.github.com>
2019-04-05 20:22:55 +0100

    PHP test

---
+++ API/test.php
@@ -0,0 +1,21 @@
+<html>
+  <head>
+    <meta name="description" content="A test API php page">
+    <meta name="author" content="stshrewsburyDev">
+    <title>stshrewsburyDev | TestAPI</title>
+    <link rel="icon" href="https://stshrewsburyDev.github.io/official_site/images/icon/MainIcon250px.png">
+    <meta property="og:title" content="stshrewsburyDev | TestAPI">
+    <meta property="og:description" content="A test API php page">
+    <meta property="og:image" content="https://stshrewsburyDev.github.io/official_site/images/icon/MainIcon250px.png">
+    <meta property="og:url" content="https://stshrewsburyDev.github.io/official_site/API/test.php">
+  </head>
+  <body>
+
+    <?php
+
+      echo "Hello world!";
+
+    ?>
+    
+  </body>
+</html>
